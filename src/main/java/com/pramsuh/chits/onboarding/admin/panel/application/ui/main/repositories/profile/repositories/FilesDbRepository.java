ded5f3fe5fd3fe16a6bfdd0f74aa3a52e7231ace
Pramsuh <pramsuhgroupindia@gmail.com>
2023-01-09 19:46:11 +0530

    deleted unwanted files

--- src/main/java/com/pramsuh/chits/onboarding/admin/panel/application/ui/main/repositories/profile/repositories/FilesDbRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pramsuh.chits.onboarding.admin.panel.application.ui.main.repositories.profile.repositories;
-
-import com.pramsuh.chits.onboarding.admin.panel.application.ui.main.models.profile.FileDB;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FilesDbRepository extends JpaRepository<FileDB, Long> {
-
-
-}
